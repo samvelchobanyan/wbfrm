391e64f5fa3467ce9209b227a92a8f71c6eef2b0
samvel <samvel.chobanyan@gmail.com>
2025-05-30 18:01:04 +0400

    asd

--- Blocks/footer.js
+++ Blocks/footer.js
@@ -2,14 +2,20 @@ import {renderBlock} from "../utils.js"
 
 export default (props) => {
         let html_str = /*html*/`        
-        <footer class="site-footer">
-          <div class="text-center">
-              2014 - Alvarez.is
-              <a href="blank.html#" class="go-top">
-                  <i class="fa fa-angle-up"></i>
-              </a>
-          </div>
-      </footer>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                            Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>.
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="text-md-right footer-links d-none d-sm-block">
+                            <a href="javascript: void(0);">About</a>
+                            <a href="javascript: void(0);">Support</a>
+                            <a href="javascript: void(0);">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         `
         return {
             fragment:renderBlock(html_str),
