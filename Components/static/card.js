7ff2bd323a8c9aa16bf93d5995a7544df91414b5
samvel <samvel.chobanyan@gmail.com>
2025-06-11 18:44:21 +0400

    asdasd

--- Components/static/card.js
+++ Components/static/card.js
@@ -1,4 +1,4 @@
-import { StaticElement } from '../../core/static-element';
+import { StaticElement } from '../../core/static-element.js';
 
 class ChartCard extends StaticElement {
   render() {
@@ -7,10 +7,10 @@ class ChartCard extends StaticElement {
         <div class="card-body">
             <h5 class="text-muted">Total Revenue</h5>
             <div class="metric-value d-inline-block">
-                <h1 class="mb-1">this.getAttribute('value')</h1>
+                <h1 class="mb-1">${this.getAttribute('value')}</h1>
             </div>
             <div class="metric-label d-inline-block float-right text-success font-weight-bold">
-                <span><i class="fa fa-fw fa-arrow-up"></i></span><span>this.getAttribute('percent')</span>
+                <span><i class="fa fa-fw fa-arrow-up"></i></span><span>${this.getAttribute('percent')}</span>
             </div>
         </div>
         <div id="sparkline-revenue"></div>
